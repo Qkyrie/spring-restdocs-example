c7b801f513085ca474673f8294e4645d7e950267
Quinten De Swaef <quintendeswaef@gmail.com>
2016-04-08 00:19:59 +0200

    removal of useless things

--- README.md
+++
@@ -1,3 +0,0 @@
-# Spring Rest Docs Example Project
-
-this is an example project on how would use spring rest docs to document restful endpoints
--- src/main/java/com/deswaef/examples/restdocs/beer/repository/BeerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.deswaef.examples.restdocs.beer.repository;
-
-import com.deswaef.examples.restdocs.beer.model.Beer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BeerRepository extends MongoRepository<Beer, String> {
-
-}
